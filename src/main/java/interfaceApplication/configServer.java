670401d464ec0a2c7f71dd60c7d846cf9c8797d1
GrapeSoftCN <jk282@163.com>
2017-05-27 09:41:59 +0800

    配置管理

--- src/main/java/interfaceApplication/configServer.java
+++ src/main/java/interfaceApplication/configServer.java
@@ -1,8 +1,10 @@
 package interfaceApplication;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import esayhelper.DBHelper;
+import esayhelper.jGrapeFW_Message;
 
 public class configServer extends DBHelper{
 	//private db serviceServer;
@@ -14,5 +16,18 @@ public class configServer extends DBHelper{
 	public String configString(String nodeName){
 		JSONObject rObject = eq("configName", nodeName).find();
 		return rObject == null ? "" : rObject.get("configjson").toString();
+	}
+	@SuppressWarnings("unchecked")
+	public String configPage(int ids,int pageSize) {
+		JSONObject obj = new JSONObject();
+		JSONArray array = page(ids, pageSize);
+		JSONObject object = new JSONObject();
+		object.put("totalSize",
+				(int) Math.ceil((double)count() / pageSize));
+		object.put("currentPage", ids);
+		object.put("pageSize", pageSize);
+		object.put("data", array);
+		obj.put("records", object);
+		return jGrapeFW_Message.netMSG(0, obj.toString());
 	}
 }
